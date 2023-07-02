e470272ecba3a555b37057e9f63cb6f6466a5623
ummu athiya <ummuathiyyah05@gmail.com>
2023-07-02 13:58:14 +0700

    chore: add color palette

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,19 @@
 module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        darkBrown: '#2b110a',
+        lightBrown: '#8a691e',
+        dustyBrown: '#605a44',
+        darkGrey: '#3b3b3c',
+        lightGrey: '#7c7c7c',
+        dustyGrey: '#7c846f',
+        cream: '#f8f1d2',
+        black: '#040404',
+        green: '#3c4431',
+      },
+    },
   },
   plugins: [],
 };
