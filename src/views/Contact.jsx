dc307769d8fb182311c82de6e043af0afaddad96
aimarmln <h.aimarmaulana@gmail.com>
2023-07-05 23:05:41 +0700

    feat: change text size on input component

--- src/views/Contact.jsx
+++ src/views/Contact.jsx
@@ -11,17 +11,22 @@ const Contact = () => {
   ];
 
   return (
-    <div className="py-12 flex justify-center items-center flex-wrap">
-      <form action="" className="mx-12 w-96">
-        {input.map(({ inputFor, isTextArea }, idx) => (
-          <InputComponent inputFor={inputFor} isTextArea={isTextArea} key={idx} />
-        ))}
-        <button class="bg-transparent hover:bg-dustyBrown text-dustyBrown font-semibold hover:text-white py-2 px-4 my-8 border border-dustyBrown hover:border-transparent transition duration-300 rounded">
-          Submit
-        </button>
-      </form>
-      <img src={require("../assets/images/for-about-page-4.jpg")} alt="test" className="w-80 mx-12" />
-    </div>
+    <>
+      <div className="py-12 flex justify-center items-center flex-wrap">
+        <form action="" className="mx-12 w-96">
+          {input.map(({ inputFor, isTextArea }, idx) => (
+            <InputComponent inputFor={inputFor} isTextArea={isTextArea} key={idx} />
+          ))}
+          <button class="bg-transparent hover:bg-dustyBrown text-dustyBrown font-semibold hover:text-white py-2 px-4 my-8 border border-dustyBrown hover:border-transparent transition duration-300 rounded">
+            Submit
+          </button>
+        </form>
+        <img src={require("../assets/images/for-about-page-4.jpg")} alt="test" className="w-80 mx-12" />
+      </div>
+      <div>
+        
+      </div>
+    </>
   );
 }
 
